db3e5f176297f5cdadad1d4220b9153c2400d33d
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-05-24 15:23:34 +0700

    fix: docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { MongooseConfigModule } from './mongoose/mongoose.module';
 import { AuthorModule } from './author/author.module';
 import { LogsModule } from './logs/logs.module';
+import { Author } from './author/entities/author.entity';
+import { AuthorController } from './author/author.controller';
+import { AuthorService } from './author/author.service';
 
 @Module({
   imports: [
@@ -13,17 +17,20 @@ import { LogsModule } from './logs/logs.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [Author],
       synchronize: true,
     }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     AuthorModule,
     LogsModule,
-    MongooseConfigModule,
-  ]
+  ],
+  controllers: [AuthorController],
+  providers: [AuthorService],
 })
 export class AppModule {}
